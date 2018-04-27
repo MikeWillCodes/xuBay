502d4006605b8b03a9d960f61ee8093a2c83af6d
Mike Williams <MikeWillCodes@gmail.com>
2018-04-26 23:19:23 -0500

    update

--- src/com/company/ItemList.java
+++ src/com/company/ItemList.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class ItemList {
     private Item head;
     private  ItemList next;
@@ -7,6 +9,16 @@ public class ItemList {
     public ItemList(Item head) {
         this.head = head;
         this.next = null;
+    }
+
+    public ItemList(int data){
+        ArrayList<>
+    }
+
+    public ItemList prepend(Item item){
+        ItemList temp = new ItemList(item);
+        temp.next = this;
+        return temp;
     }
 
     public ItemList add(Item item){
@@ -23,5 +35,13 @@ public class ItemList {
 
     public ItemList set(int index, Item item){
 
+    }
+
+    @Override
+    public String toString() {
+        return "ItemList{" +
+                "head=" + head +
+                ", next=" + next +
+                '}';
     }
 }
