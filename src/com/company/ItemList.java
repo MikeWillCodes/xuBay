6f30c580342bc3c901c9134cbfd72767c09b66bb
Mike Williams <MikeWillCodes@gmail.com>
2018-04-26 22:45:20 -0500

    Initial commit

---
+++ src/com/company/ItemList.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class ItemList {
+    private Item head;
+    private  ItemList next;
+
+    public ItemList(Item head) {
+        this.head = head;
+        this.next = null;
+    }
+
+    public ItemList add(Item item){
+
+    }
+
+    public  ItemList size(){
+
+    }
+
+    public ItemList get(Item item){
+
+    }
+
+    public ItemList set(int index, Item item){
+
+    }
+}
