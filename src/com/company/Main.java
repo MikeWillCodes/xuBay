42f08851bf25dc60da86b42ee76eb31df4b9c28a
Mike Williams <MikeWillCodes@gmail.com>
2018-05-06 11:43:21 -0500

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,24 @@
 package com.company;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\nWelcome to XUBay\n");
 
         ItemList items = new ItemList(5);
+
+        Item sell = new Item(900199999,"Nate",
+                50,2, LocalDate.now().plusDays(3));
+        System.out.println("New Seller: " + sell);
+
         System.out.println("Is Item List empty?: " + items.isEmpty());
+
         System.out.println("The size of listings: " + items.size());
+
         System.out.println("Get [index]: " + items.get(4));
+
         System.out.println("Listings: " + items);
 
 
